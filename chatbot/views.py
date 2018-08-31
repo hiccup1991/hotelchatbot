25a85d9c98fd14ac3709a70b2cb1faf3914e5fb0
hiccup1991 <hiccup1991@hotmail.com>
2018-08-31 13:31:10 +0900

    show chat history

--- chatbot/views.py
+++ chatbot/views.py
@@ -13,19 +13,27 @@ def select_room(request):
 
 @login_required
 def frontdesk(request):
-    return render(request, 'chatbot/frontdesk.html')
+    histories = History.objects.filter(chatdatetime__lte=timezone.now()).filter(user=request.user).filter(bot=Bot.objects.get(name="frontdesk")).order_by('chatdatetime')
+    return render(request, 'chatbot/frontdesk.html', {'histories': histories})
+    # return render(request, 'chatbot/frontdesk.html')
 
 @login_required
 def concierge(request):
-    return render(request, 'chatbot/concierge.html')
+    histories = History.objects.filter(chatdatetime__lte=timezone.now()).filter(user=request.user).filter(bot=Bot.objects.get(name="concierge")).order_by('chatdatetime')
+    return render(request, 'chatbot/concierge.html', {'histories': histories})
+    # return render(request, 'chatbot/concierge.html')
 
 @login_required
 def activitiesdesk(request):
-    return render(request, 'chatbot/activitiesdesk.html')
+    histories = History.objects.filter(chatdatetime__lte=timezone.now()).filter(user=request.user).filter(bot=Bot.objects.get(name="activitiesdesk")).order_by('chatdatetime')
+    return render(request, 'chatbot/activitiesdesk.html', {'histories': histories})
+    # return render(request, 'chatbot/activitiesdesk.html')
 
 @login_required
 def reservations(request):
-    return render(request, 'chatbot/reservations.html')
+    histories = History.objects.filter(chatdatetime__lte=timezone.now()).filter(user=request.user).filter(bot=Bot.objects.get(name="reservations")).order_by('chatdatetime')
+    return render(request, 'chatbot/reservations.html', {'histories': histories})
+    # return render(request, 'chatbot/reservations.html')
 
 @login_required
 def frontdeskask(request):
@@ -41,3 +49,44 @@ def frontdeskask(request):
         instance = History.objects.create(user=request.user, bot=Bot.objects.get(name="frontdesk"), usertext=message, bottext=bot_response)
         return JsonResponse({'status':'OK','answer':bot_response})
 
+@login_required
+def conciergeask(request):
+    message = request.POST.get("messageText", "")
+    kernel = aiml.Kernel()
+    if os.path.isfile("bot_brain.brn"):
+        kernel.bootstrap(brainFile = "bot_brain.brn")
+    else:
+        kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+        kernel.saveBrain("bot_brain.brn")
+    while True:
+        bot_response = kernel.respond(message)
+        instance = History.objects.create(user=request.user, bot=Bot.objects.get(name="concierge"), usertext=message, bottext=bot_response)
+        return JsonResponse({'status':'OK','answer':bot_response})
+
+@login_required
+def activitiesdeskask(request):
+    message = request.POST.get("messageText", "")
+    kernel = aiml.Kernel()
+    if os.path.isfile("bot_brain.brn"):
+        kernel.bootstrap(brainFile = "bot_brain.brn")
+    else:
+        kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+        kernel.saveBrain("bot_brain.brn")
+    while True:
+        bot_response = kernel.respond(message)
+        instance = History.objects.create(user=request.user, bot=Bot.objects.get(name="activitiesdesk"), usertext=message, bottext=bot_response)
+        return JsonResponse({'status':'OK','answer':bot_response})
+
+@login_required
+def reservationskask(request):
+    message = request.POST.get("messageText", "")
+    kernel = aiml.Kernel()
+    if os.path.isfile("bot_brain.brn"):
+        kernel.bootstrap(brainFile = "bot_brain.brn")
+    else:
+        kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+        kernel.saveBrain("bot_brain.brn")
+    while True:
+        bot_response = kernel.respond(message)
+        instance = History.objects.create(user=request.user, bot=Bot.objects.get(name="reservations"), usertext=message, bottext=bot_response)
+        return JsonResponse({'status':'OK','answer':bot_response})
